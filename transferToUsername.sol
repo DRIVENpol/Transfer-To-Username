40e3123a4e1da6e9b2c70702f1ce0356d2a7f5af
DRIVENpol <84129217+DRIVENpol@users.noreply.github.com>
2022-03-05 01:40:42 +0200

    Update transferToUsername.sol

--- transferToUsername.sol
+++ transferToUsername.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
-contract TransferToUsername is Ownable {
+contract TransferToUsername {
 
 using SafeMath for uint256;
 
 
 // VARIABLES
-uint256 public feeOnTransfer;
+//uint256 public feeOnTransfer;
 uint256 public feeSetUsername;
 
 // MAPPINGS
@@ -57,12 +57,13 @@ function _transferEthFromContract(address _toUser, uint256 _amount) internal {
 }
 
 // PUBLIC FUNCTIONS
-function setMyUsername(string memory newUsername) public payable {
+function setMyUsername(string memory newUsername) public {
 _setMyUsername(newUsername, msg.sender);
 emit SetUsername(newUsername, msg.sender);
 }
 
-function sendToUSername(string memory toUser, address tokenAddress, uint256 amount) public payable {
+function sendToUsername(string memory toUser, address tokenAddress, uint256 amount) public {
+    require(usernameExist(toUser), "The username don't exist!");
     address _toUser = usernameToAddress[toUser];
     address _fromUser = msg.sender;
     _transferToContract(_fromUser, tokenAddress, amount);
@@ -72,8 +73,23 @@ function sendToUSername(string memory toUser, address tokenAddress, uint256 amou
 
 function sendEthToUsername(string memory toUser, uint256 amount) public payable {
   //  require(msg.value >= feeOnTransfer, "Pay the fee before transfer!");
+    require(usernameExist(toUser), "The username don't exist!");
     address _toUser = usernameToAddress[toUser];
     _transferEthToContract(amount);
     _transferEthFromContract(_toUser, amount);
 }
+
+// GETTERS
+function getMyUsername() public view returns(string memory) {
+    return addressToUsername[msg.sender];
+}
+
+function usernameExist(string memory toUser) public view returns(bool) {
+if(usernameToAddress[toUser] != address(0x0)) {
+return true;
+} else {
+    return false;
+    }
+}
+
 }
